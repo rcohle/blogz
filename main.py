1b0a512d8de4a0ff3007d791e65e68a4a7ea8d0b
Rosanna Cohle <rosanna76@gmail.com>
2017-12-15 05:39:51 -0600

    aesthetics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template,url_for
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,8 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+###########################################################################
 
 class Blog(db.Model):
 
@@ -17,29 +19,32 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-# redirects from 'home page' to blog page
+###########################################################################
+# redirect from 'home page' to blog page
 @app.route('/')
 def index():
     return redirect('/blog')
 
-# displays all blogs
+###########################################################################
+# display all blogs
 @app.route('/blog', methods=['POST', 'GET'])
 def blog():
 
     blog_posts = Blog.query.all()
     return render_template('/blog.html', blog_posts=blog_posts)
 
-# displays posts individually
+###########################################################################
+# display posts individually
 @app.route('/post')
 def post():
     id = request.args.get('id')
     post_id = Blog.query.get(id)
     return render_template('/post.html',post_id=post_id)
 
-# allow user to add a new post
+###########################################################################
+# add new post
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
-
     error_title = 'Please enter a title for your blog post'
     error_body = 'Please enter content for your blog post'
     error_empty_title = ''
@@ -54,25 +59,11 @@ def new_post():
             error_empty_body = error_body
 
     if request.method == 'POST' and error_empty_body == '' and error_empty_title == '':
-        
         new_post = Blog(title,body)
-
         db.session.add(new_post)
         db.session.commit()
-        
-        #post_id1 = request.args.get('id')
-        
         post_id = Blog.query.get(new_post.id)
-        
-        #post_id = Blog.query.filter_by().first()
-        #post_id = Blog.query.get(id)
-        #post_id = Blog.query.filter_by(id=id).first()
-        
-        #return redirect('/post')
-        #return redirect(url_for('post', post_id=post_id))
-        
         return render_template('post.html', post_id=post_id)
-
     else:
         return render_template('/newpost.html',error_empty_title=error_empty_title,error_empty_body=error_empty_body)
 
